06d4caa872ef84c5206386778a850fc5587c26ae
LucasLourencetti <llourencetti72@gmail.com>
2023-11-24 16:15:27 -0300

    24-11

---
+++ Petshop1/Controllers/ConsultaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Petshop1.Models;
+
+namespace Petshop1.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        private readonly Contexto contexto;
+
+        public ConsultaController(Contexto context)
+        {
+            contexto = context;
+        }
+
+        public IActionResult Filtrar()
+        {
+            return View();
+        }
+
+        public IActionResult conServ(string busca)
+        {
+            var serv = contexto.Servicos.Include(cli => cli.Cliente)
+                                        .Include(ani => ani.Animal)
+                                        .Include(tip => tip.TipoServico)
+                                        .Where(o => o.Animal.nome == busca)
+                                        .ToList();
+            return View(serv);
+        }
+    }
+}
